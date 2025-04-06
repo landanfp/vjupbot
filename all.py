0b29d41a9ad74d358225f8157c2ff319acfb0974
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-06 15:13:29 +0330

    Update all.py

--- all.py
+++ all.py
@@ -1424,3 +1424,193 @@ def DownLoadFile(url, file_name, chunk_size, client, ud_type, message_id, chat_i
                     except:
                         pass
     return file_name
+
+# database/users_chats_db.py file : 
+import motor.motor_asyncio
+
+from config import Config
+
+
+
+DATABASE_NAME = "vjbotztechvj"
+
+DATABASE_URI = Config.TECH_VJ_DATABASE_URL
+
+
+
+class Database:
+
+    
+
+    def __init__(self, uri, database_name):
+
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+
+        self.db = self._client[database_name]
+
+        self.col = self.db.users
+
+        self.grp = self.db.groups
+
+
+
+
+
+    def new_user(self, id, name):
+
+        return dict(
+
+            id = id,
+
+            name = name,
+
+            ban_status=dict(
+
+                is_banned=False,
+
+                ban_reason="",
+
+            ),
+
+        )
+
+
+
+
+
+    def new_group(self, id, title):
+
+        return dict(
+
+            id = id,
+
+            title = title,
+
+            chat_status=dict(
+
+                is_disabled=False,
+
+                reason="",
+
+            ),
+
+        )
+
+    
+
+    async def add_user(self, id, name):
+
+        user = self.new_user(id, name)
+
+        await self.col.insert_one(user)
+
+    
+
+    async def is_user_exist(self, id):
+
+        user = await self.col.find_one({'id':int(id)})
+
+        return bool(user)
+
+    
+
+    async def total_users_count(self):
+
+        count = await self.col.count_documents({})
+
+        return count
+
+    
+
+    
+
+
+
+    async def get_all_users(self):
+
+        return self.col.find({})
+
+    
+
+
+
+    
+
+
+
+
+
+    async def get_db_size(self):
+
+        return (await self.db.command("dbstats"))['dataSize']
+
+
+
+
+
+tech_vj = Database(DATABASE_URI, DATABASE_NAME)
+
+
+
+# database/database.py file :
+import datetime
+import motor.motor_asyncio
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.clinton = self._client[database_name]
+        self.col = self.clinton.USERS
+
+    def new_user(self, id):
+        return dict(id=id, thumbnail=None)
+
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_thumbnail(self, id, thumbnail):
+        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail', None)
+
+# database/adduser.py :
+from pyrogram import Client
+from database.access import techvj
+from pyrogram.types import Message
+from config import Config
+
+LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Nᴀᴍᴇ - {}"""
+
+
+async def AddUser(bot: Client, update: Message):
+    if not await techvj.is_user_exist(update.from_user.id):
+           await techvj.add_user(update.from_user.id)
+           await bot.send_message(Config.TECH_VJ_LOG_CHANNEL, LOG_TEXT_P.format(update.from_user.id, update.from_user.mention))
+
+# database/access.py file :
+from config import Config
+from database.database import Database
+
+techvj = Database(Config.TECH_VJ_DATABASE_URL, Config.TECH_VJ_SESSION_NAME)
