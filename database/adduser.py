ef00c1c6b3aacbc8b571cc1aa1ffb9c2e087360c
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-06 12:49:25 +0330

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client
 from database.access import techvj
 from pyrogram.types import Message
